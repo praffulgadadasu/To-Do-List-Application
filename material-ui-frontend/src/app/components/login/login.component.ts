95eb52a2a54be1b58139903c458ed70e5c66581b
praffulgadadasu <praffultheja@gmail.com>
2023-01-31 22:25:51 +1100

    CORS Policy

--- material-ui-frontend/src/app/components/login/login.component.ts
+++ material-ui-frontend/src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { FrontendService } from 'src/app/services/frontend.service';
 
 @Component({
   selector: 'app-login',
@@ -9,29 +10,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  
   public loginForm !: FormGroup;
-  constructor( private formBuilder : FormBuilder, private http: HttpClient, private router: Router ) { }
+  constructor( private formBuilder : FormBuilder, private http: HttpClient, private router: Router, private frontEndService: FrontendService ) { }
+ 
   ngOnInit(): void{
     this.loginForm = this.formBuilder.group({
       email:['', Validators.required],
       password:['', Validators.required]
     })
   }
+  
   login(){
-    this.http.get<any>("http://localhost:3000/api/v1/auth/login")
-    .subscribe(res=>{
-      const user = res.find((a:any)=>{
-        return a.email == this.loginForm.value.email && a.password == this.loginForm.value.password
-      });
-      if(user){
-        alert("User Logged In Successfully!");
-        this.loginForm.reset();
-        this.router.navigate(['home'])
-      }else{
-        alert("Invalid Credentials!")
-      }
-    },err=>{
-      alert("Something went wrong. Try Again Later!")
+    this.http.post<any>("http://localhost:3000/api/v1/auth/login", this.loginForm.value)
+    console.log(this.loginForm.value)
+    this.frontEndService.signIn(this.loginForm.value).subscribe(res =>{
+      console.log(res);
     })
   }
 }
--- material-ui-frontend/src/app/services/frontend.service.ts
+++ material-ui-frontend/src/app/services/frontend.service.ts
@@ -1,31 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
-import { UserModel } from '../shared/models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FrontendService {
 
- /*  userProfile: BehaviorSubject<UserModel> = new BehaviorSubject<UserModel>({
-    id:'',
-    username:'',
-    email:''
-  })
- */
   constructor(private http: HttpClient) { }
   signIn( user:any){
-    return this.http.post('http://localhost:3000/api/v1/auth/login', user, { withCredentials:true});
+    return this.http.post('http://localhost:3000/api/v1/auth/login', user);
   }
-
-  /* profile(){
-    return this.http.get<UserModel>('http://localhost:3000/api/v1/user-profile',{
-      withCredentials:true
-    });
-  }
-  saveUserToLocalStorage(user:UserModel){
-    this.userProfile.next(user);
-    localStorage.setItem('user-profile', JSON.stringify(user));
-  } */
 }
